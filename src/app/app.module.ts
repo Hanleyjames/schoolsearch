caa79b7bc89a833c3a121e984c2e9925fae8bcf3
Hanleyjames <hanley.doth@outlook.com>
2019-06-05 09:55:24 -0700

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TopnavComponent } from './components/navbar/topnav/topnav.component';
+import { BottomnavComponent } from './components/navbar/bottomnav/bottomnav.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdvancesearchComponent } from './components/advancesearch/advancesearch.component';
+import { SearchresultsComponent } from './components/searchresults/searchresults.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TopnavComponent,
+    BottomnavComponent,
+    HomeComponent,
+    AdvancesearchComponent,
+    SearchresultsComponent
   ],
   imports: [
     BrowserModule,
