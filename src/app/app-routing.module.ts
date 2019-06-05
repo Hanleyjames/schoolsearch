48785f51c518956ba26cf394c055455a79f9d5f0
Hanleyjames <hanley.doth@outlook.com>
2019-06-05 10:16:04 -0700

    Add routes to app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { HomeComponent } from './components/home/home.component';
 import { SearchresultsComponent } from './components/searchresults/searchresults.component';
 import { AdvancesearchComponent } from './components/advancesearch/advancesearch.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '/results', component: SearchresultsComponent },
+  { path: '/search', component: AdvancesearchComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
