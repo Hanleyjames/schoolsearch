94d2d9c1264025429eefdb5e0b38440e3e496b79
Hanleyjames <hanley.doth@outlook.com>
2019-06-05 10:10:15 -0700

    Import components into routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchresultsComponent } from './components/searchresults/searchresults.component';
+import { AdvancesearchComponent } from './components/advancesearch/advancesearch.component';
 
 const routes: Routes = [];
 
