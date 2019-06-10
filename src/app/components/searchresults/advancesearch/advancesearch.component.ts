c49a36f6bdd7bdb93c29d2be1a9b15f061cef9a4
Hanleyjames <hanley.doth@outlook.com>
2019-06-10 16:38:27 -0700

    Restructured some things. partial json rework

--- src/app/components/searchresults/advancesearch/advancesearch.component.ts
+++ src/app/components/searchresults/advancesearch/advancesearch.component.ts
@@ -13,19 +13,40 @@ export class AdvancesearchComponent implements OnInit {
     private ref: ChangeDetectorRef) { }
   careerField: object;
   academicSubjects: object;
+  academicSubjectsArray: string[];
+  tempIndex: string;
+  tempIndex2: string;
+  tempCareerField: string;
+  tempAcademicSubject: string;
+  currentCareerField: string;
+  currentAcademicSubject: string;
 
+  isEmpty(obj) {
+    for (const key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  storeIndex() {
+    const i = (document.getElementById('firstDropDown') as HTMLInputElement).value;
+    this.tempIndex = i;
+
+  }
 
 
   ngOnInit() {
     this.httpService.get('../../../assets/json/classes.json').subscribe(
       data => {
         this.careerField = data;
+        console.log(data);
       },
       (err: HttpErrorResponse) => {
         console.log(err.message);
       }
     );
-    console.log(this.careerField);
+
   }
 
 }
