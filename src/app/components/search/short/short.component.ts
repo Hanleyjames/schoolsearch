d7da08bec2544e311e93d883bed8a8e8e28408c7
Hanleyjames <hanley.doth@outlook.com>
2019-06-12 11:13:16 -0700

    Document short

--- src/app/components/search/short/short.component.ts
+++ src/app/components/search/short/short.component.ts
@@ -10,6 +10,7 @@ export class ShortComponent implements OnInit {
 
   constructor(private httpService: HttpClient,
     private ref: ChangeDetectorRef) { }
+  //type definitions
   data: object;
   careers: object;
   subjects: object;
@@ -24,22 +25,35 @@ export class ShortComponent implements OnInit {
     const i = (document.getElementById('careerDropDown') as HTMLInputElement).value;
     //Set subjects to the strand object
     this.subjects = this.data[i].strand;
-
+    //sets TempCareers to the name
     this.tempCareers = this.data[i].name;
+    //sets tempSubject to a blank array
     this.tempSubject = [];
+    //Object.entries returns an array of a given own enumerated key pair properties of the data.strand
     Object.entries(this.data[i].strand).forEach(entry => {
+      //Gets the name of the subject
       let key = entry[0];
+      //Gets the nested object of the subject
       let value = entry[1];
+      //A for each loop iterated over each object of the subject(Clinical Engineering)
       Object.entries(value).forEach(entry => {
+        //iterates over strand name key, outcome key, and subjects(Mat, Sci...) key
         let key = entry[0];
+        //iterates over strand name values, outcomes values, and subjects objects
         let value = entry[1];
         let x = [key, value];
-        console.log("Key & value: "+x)
+
       })
-      console.log("Key: "+key);
+      //pushes the subject names into an array
+      console.log(key);
       this.tempSubject.push(key);
     })
   }
+  //write setter
+  setSearchTerms(name, strand, subjects) {
+
+  }
+  //finish emitter to results
 
   ngOnInit() {
     this.httpService.get('../../../assets/json/classes.json').subscribe(
