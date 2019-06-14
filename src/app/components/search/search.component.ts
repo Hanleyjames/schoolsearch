62367718b3904e91184a5bb406611c2384868997
Hanleyjames <hanley.doth@outlook.com>
2019-06-14 13:59:39 -0700

    Write part to receive organized data to the search parent from the short sibling using services

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchdropdownService } from '../../_services/searchdropdown.service';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  message: object;
 
-  constructor() { }
+  constructor(private data: SearchdropdownService) { }
 
   ngOnInit() {
+    this.data.currentMessage.subscribe(message => this.message = message);
+    console.log(this.message);
   }
+
+
 
 }
